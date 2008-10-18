633cbd6460899b233ff56c6219a7d198b4aeb92c
Manuel <manuel.meurer@gmail.com>
2008-10-18 16:00:50 +0200

    Added possibility to use a regex in if_var and unless_var tags

--- lib/parameterized_snippets/page_ext.rb
+++ lib/parameterized_snippets/page_ext.rb
@@ -5,47 +5,54 @@ module ParameterizedSnippets
 
     desc %{
       Renders the containing elements only if the snippet was called with a certain parameter.
+      If an additional "matches" parameter containing a regular expression is used, the containing
+      elements are only rendered if the parameter exists and matches the regular expression.
       
-      *Usage:* 
+      *Usage:*
       
-      In page "Home":
+      <pre><code><r:if_var name="parameter_name" [matches="regex"]>...</r:if_var></code></pre>
+      
+      *Example:*
+      
+      Some page:
       
       <pre><code><r:snippet name="animal_info" animal="elephant" /></code></pre>
 
-      In snippet "animal_info":
+      animal_info snippet:
       
-      <pre><code><r:if_var name="animal">...</r:if_var></code></pre>
+      <pre><code><r:if_var name="animal" matches=".le(?:ph|f)ant">...</r:if_var></code></pre>
     }
     tag "snippet:if_var" do |tag|
-      var = check_for_attr(tag, 'name')
-      content = get_content_from_tag_binding(tag)
-      tag.expand if content.attr[var]
+      tag.expand if var_exists_and_matches(tag)
     end
 
     desc %{
       The opposite of the @if_var@ tag.
       
-      *Usage:* 
-      <pre><code><r:unless_var name="animal">...</r:unless_var></code></pre>
+      *Usage:*
+      
+      <pre><code><r:unless_var name="parameter_name" [matches="regex"]>...</r:unless_var></code></pre>
     }
     tag "snippet:unless_var" do |tag|
-      var = check_for_attr(tag, 'name')
-      content = get_content_from_tag_binding(tag)
-      tag.expand unless content.attr[var]
+      tag.expand unless var_exists_and_matches(tag)
     end
 
     desc %{
       Outputs the value of the parameter or an error message if the parameter cannot be found.
       
-      *Usage:* 
+      *Usage:*
+
+      <pre><code><r:var name="parameter_name" /></code></pre>
       
-      In page "Home":
+      *Example:*
+      
+      Some page:
       
       <pre><code><r:snippet name="animal_info" animal="elephant" /></code></pre>
 
-      In snippet "animal_info":
+      animal_info snippet:
       
-      <pre><code><r:var name="animal" /></code></pre>
+      <pre><code><r:var name="animal" /> # Outputs 'elephant'</code></pre>
     }
     tag "snippet:var" do |tag|
       var = check_for_attr(tag, 'name')
@@ -69,6 +76,16 @@ module ParameterizedSnippets
 
       def get_content_from_tag_binding(tag)
         tag.context.instance_variable_get(:@tag_binding_stack).detect { |slot| slot.name == 'snippet' }
+      end
+
+      def var_exists_and_matches(tag)
+        var = check_for_attr(tag, 'name')
+        content = get_content_from_tag_binding(tag)
+        if tag.attr['matches']
+          content.attr[var] =~ /^#{tag.attr['matches']}$/
+        else
+          content.attr[var]
+        end
       end
 
   end
